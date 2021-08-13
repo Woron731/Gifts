13402d4aefd32695462b5491a66e8d75c4c0059b
woron731 <woronmsk@mail.ru>
2021-08-13 20:53:16 +0300

    js cute version

--- js/script.js
+++ js/script.js
@@ -4,27 +4,21 @@ let offerItem = document.querySelectorAll('.offer__item');
 
 let offerPrice = document.querySelectorAll('.offer__item-price');
 
-let arrayofferPrice = Array.from(offerPrice);
-
 let offerAuthor = document.querySelectorAll('.offer__item-author');
 
-let arrayofferAuthor = Array.from(offerAuthor);
-
 let offerButton = document.querySelectorAll('.offer__item-button');
-
-let arrayofferButton = Array.from(offerButton);
 
 offerItem.forEach((item, index) => {
 
 	item.addEventListener('mouseover', function () {
-		arrayofferPrice[index].classList.add('hidden');
-		arrayofferAuthor[index].classList.add('hidden');
-		arrayofferButton[index].classList.remove('hidden');
+		offerPrice[index].classList.add('hidden');
+		offerAuthor[index].classList.add('hidden');
+		offerButton[index].classList.remove('hidden');
 	})
 
 	item.addEventListener('mouseout', function () {
-		arrayofferPrice[index].classList.remove('hidden');
-		arrayofferAuthor[index].classList.remove('hidden');
-		arrayofferButton[index].classList.add('hidden');
+		offerPrice[index].classList.remove('hidden');
+		offerAuthor[index].classList.remove('hidden');
+		offerButton[index].classList.add('hidden');
 	})
 })
